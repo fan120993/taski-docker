cdb22efd8ede732b5066550ce8f79474dd2a6c9c
Vladimir Filippov <fanlegenda@list.ru>
2026-03-18 14:11:41 +0300

    Add workflow with PEP8 linting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,8 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '123.123.123.123', 'fanserv.ddns.net', '127.0.0.1'] 
-
+ALLOWED_HOSTS = ['localhost', '123.123.123.123', 'fanserv.ddns.net', '127.0.0.1']
 
 # Application definition
 
